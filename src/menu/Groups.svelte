c6ba54180ba1d65e6898688a8e36740952bdda28
Ondrej Spanik <spanik11@msn.com>
2020-09-27 15:37:28 +0200

    Fixed Groups from object-type to new array-type implementation

--- src/menu/Groups.svelte
+++ src/menu/Groups.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <div id="groups">
-{#each $groups as group}
+{#each $groups as group, i}
     <details class="group">
         <summary>
-            <input type="text" placeholder="Group" value={group.name}>
+            <input type="text" placeholder={"Group " + (i + 1)} value={group[4]}>
         </summary>
         <div>
-            <input type="text" name="prefix" id="group_n_prefix" placeholder="Prefix" value={group.prefix}><br>
-            <input type="text" name="suffix" id="group_n_suffix" placeholder="Suffix" value={group.suffix}>
+            <input type="text" name="prefix" id="group_n_prefix" placeholder="Prefix" value={group[5]}><br>
+            <input type="text" name="suffix" id="group_n_suffix" placeholder="Suffix" value={group[6]}>
         </div>
     </details>
 {/each}
