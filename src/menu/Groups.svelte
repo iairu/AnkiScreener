24d7113ceb273f4081dbb4efefdc7796e2757889
Ondrej Spanik <spanik11@msn.com>
2020-10-03 01:42:41 +0200

    Bugfix: Spacebar in summary input triggering details toggle

--- src/menu/Groups.svelte
+++ src/menu/Groups.svelte
@@ -1,5 +1,8 @@
 <script>
-    import { groups, modifySelection } from "../store";
+    import { groups, modifySelection, notify } from "../store";
+
+    let lastDetailsKeyCode = 0;
+    let summaryInputActive = false;
 
     function handleGroupName(e) {
         modifySelection(true,parseInt(e.target.getAttribute("key")),{name:e.target.value});
@@ -10,14 +13,20 @@
     function handleGroupSuffix(e) {
         modifySelection(true,parseInt(e.target.getAttribute("key")),{suffix:e.target.value});
     }
+    function preventDetailsToggle(e) {
+        // bugfix for spacebar in summary's input box triggering details toggle
+        if (lastDetailsKeyCode === 32 && summaryInputActive) {
+            lastDetailsKeyCode = 0; // prevent infinite loop
+            e.target.open = !e.target.open; // e.preventDefault() doesn't work here
+        }
+    }
 </script>
 
 <div id="groups">
 {#each $groups as group, i}
-    <details class="group">
+    <details class="group" on:toggle={preventDetailsToggle} on:keydown={(e)=>{lastDetailsKeyCode = e.keyCode}}>
         <summary>
-            <!-- todo: Spacebar is triggering both the character and details rollup/down toggle -->
-            <input type="text" key={i} placeholder={"Group " + (i + 1)} value={group.name} on:input={handleGroupName}>
+            <input type="text" key={i} placeholder={"Group " + (i + 1)} value={group.name} on:input={handleGroupName} on:blur={()=>{summaryInputActive = false}} on:focus={()=>{summaryInputActive = true}}>
         </summary>
         <div>
             <input type="text" key={i} name="prefix" id="group_n_prefix" placeholder="Prefix" value={group.prefix} on:input={handleGroupPrefix}><br>
